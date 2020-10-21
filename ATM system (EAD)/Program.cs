abeb82aacca4917a048354b828a8e9ca9ca6978d
umair-j <umair-j@github.com>
2020-10-21 17:41:11 +0500

    Add project files.

---
+++ ATM system (EAD)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ATM_system__EAD_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
